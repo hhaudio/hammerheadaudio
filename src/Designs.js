8d594f9ce9870778060d779203736a61402a6249
hhaudio <emmett@hammerheadaudio.com>
2019-11-06 18:23:07 -0700

    Updated portfolio + resume sections

--- src/Designs.js
+++ src/Designs.js
@@ -23,6 +23,19 @@ var cigText = "The CigSynth OWKY-1 (One Won\'t Kill Ya!) is a handmade, battery-
 var cigVid = 'https://www.youtube.com/watch?v=eb6wKSP1cwg';
 var cigLink = "https://github.com/hhaudio/CigSynth";
 
+var ne1Text = "The Nuisance Engine (NE-1) is a handmade, 9V battery powered, analog pocket synthesizer unit. Modeled after a circuit that emulates the sounds of a V8 motor, the Nuisance Engine is an abrasive noise box with a mind of its own, capturing the coveted quality of unpredictable yet clear results from user input. The signal flow consists of a square wave oscillator exciting a variable frequency resonator filter, which then passes through a combination lowpass filter / distortion circuit that cranks in incredible amounts of gain as the filter cutoff decreases, creating a decidedly smushy, crushy end result. This all runs into an audiophile unfriendly Class B amplifier circuit so cheap that it ducks the power supply of the entire device in the process of driving the built-in speaker, leading to some weird, chaotic feedback magic that makes the NE-1 the Nuisance it is. Perfect for irritating your friends and enemies on the go...";
+var ne1Vid = 'https://youtu.be/uStgD_TsTdg';
+var ne1Link = "https://igg.me/at/ne1/x/22645203#/";
+var ne1Sub = "Enclosure Art By Suzanne Yeremyan";
+
+var pn1Text = "Essentially a pocket jackhammer, the Puncher Noid (PN-1) is an lfo controlled solenoid boxing glove powered off two 9 Volt Batteries. Additional features include wall wart power input, as well as an external trigger input, which allows the glove to be controlled by external waveforms from modular synths, etc..."
+var pn1Vid = 'https://youtu.be/Pm1rNjtYY48';
+var pn1Link = "default";
+
+var pn2Text = "The Pincer Noids (PN-2) is a battery powered pocket lobster, featuring two clacking solenoid claws. Turning the rotary encoder control produces sequenced clacking, while pressing the button fires both at once. Also included is a trigger input, which allows the claws to be controlled by externally generated waveforms from modular synths, etc...";
+var pn2Vid = 'https://youtu.be/MkUJVbHedGs';
+var pn2Link = "default";
+
 var srmText = 'Say something about SharkBite Ring Modulator';
 var srmVid = 'https://www.youtube.com/watch?v=eb6wKSP1cwg';
 var srmLink = "none";
@@ -32,7 +45,7 @@ class Designs extends Component {
       super(props);
 
       this.state = {
-        design: "Ghost Fax"
+        design: "Nuisance Engine"
       }
     }
 
@@ -54,6 +67,17 @@ class Designs extends Component {
           </div>
         </div>
         );
+    }
+
+    inquireDiv(text){
+      return (
+      <div>
+          {text}
+          <div id="Purchase-NoLink">
+            If interested in purchase or samples, please inquire at: <bold style={{textDecoration: "underline"}}>emmett@hammerheadaudio.com</bold> 
+          </div>
+        </div>  
+      );
     }
 
     navClicked(page) {
@@ -88,15 +112,21 @@ class Designs extends Component {
 			   <div style={{display: this.props.display}} className="Content-Container">
 	    		<div className="Content-Text-Box">
             <div className="DesignBar">
-  	    			<DesignMenu text="Ghost Fax" idfl="First-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
+              <DesignMenu text="Nuisance Engine" idfl="First-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
+  	    			<DesignMenu text="Ghost Fax" idfl="Middle-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
               <DesignMenu text="Hax II" idfl="Middle-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
-              <DesignMenu text="The Abyss" idfl="Middle-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
-              <DesignMenu text="CigSynth" idfl="Last-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
+              <DesignMenu text="Pincer Noids" idfl="Middle-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
+              <DesignMenu text="Puncher Noid" idfl="Middle-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
+              <DesignMenu text="CigSynth" idfl="Middle-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
+              <DesignMenu text="The Abyss" idfl="Last-Item" nav={this.state.design} onClick={this.navClicked.bind(this)}/>
             </div>
             <DesignContent designText={abyssText} vidUrl={abyssVid} subText={abyssSub} buyLink={abyssLink} display={this.state.design === "The Abyss" ? 'block' : 'none'} playerWidth={playerWidth} playerHeight={playerHeight}/>
             <DesignContent designText={this.gfaxDiv()} subText={gFaxSub} vidUrl={gFaxVid} buyLink={gFaxLink} display={this.state.design === "Ghost Fax" ? 'block' : 'none'} playerWidth={playerWidth} playerHeight={playerHeight}/>
 	    	    <DesignContent designText={haxText} vidUrl={haxVid} subText={haxSub} buyLink={haxLink} display={this.state.design === "Hax II" ? 'block' : 'none'} playerWidth={playerWidth} playerHeight={playerHeight}/>
             <DesignContent designText={this.cigDiv()} vidUrl={cigVid} buyLink={cigLink} buyLinkText="Build Manual" buy display={this.state.design === "CigSynth" ? 'block' : 'none'} playerWidth={playerWidth} playerHeight={playerHeight}/>
+            <DesignContent designText={ne1Text} vidUrl={ne1Vid} subText={ne1Sub} buyLink={ne1Link} buyLinkText="Preorder Now" display={this.state.design === "Nuisance Engine" ? 'block' : 'none'} playerWidth={playerWidth} playerHeight={playerHeight}/>
+            <DesignContent designText={pn1Text} vidUrl={pn1Vid} buyLink={pn1Link} display={this.state.design === "Puncher Noid" ? 'block' : 'none'} playerWidth={playerWidth} playerHeight={playerHeight}/>
+            <DesignContent designText={pn2Text} vidUrl={pn2Vid} buyLink={pn2Link} display={this.state.design === "Pincer Noids" ? 'block' : 'none'} playerWidth={playerWidth} playerHeight={playerHeight}/>
             <DesignContent designText={srmText} vidUrl={srmVid} buyLink={srmLink} display={this.state.design === "SRM-1" ? 'block' : 'none'} playerWidth={playerWidth} playerHeight={playerHeight}/>
           </div>
 	    	</div>
--- src/Resume.js
+++ src/Resume.js
@@ -11,135 +11,65 @@ class Resume extends Component {
     				<a className="Resume-Text" id="LinkedIn" style={{left: "0px"}} href="EmmettPalaima_Resume.pdf" download>Download PDF</a>
     			</div>
     			<div className="Resume-Section">
+    				<div className="Resume-Section-Header">Professional Summary</div>
+    				<div className="Resume-Text">Artist, musician, programmer and electrical engineer, specializing in the creative application of
+audio technology. Experience in installation art, product design, and performance. Excellent
+technical, problem solving, and creative skills.</div>
+    			</div>
+    			<div className="Resume-Section">
+    				<div className="Resume-Section-Header">Skills</div>
+	    				<div className="Resume-Text">Electrical Engineering</div>
+	    				<div className="Resume-Text">Circuit Board Design</div>
+	    				<div className="Resume-Text">Software and Firmware Programming</div>
+	    				<div className="Resume-Text">Digital Signal Processing</div>
+	    				<div className="Resume-Text">Digital Fabrication</div>
+	    				<div className="Resume-Text">Creative Audio Design</div>
+	    				<div className="Resume-Text">Sound Design</div>
+	    				<div className="Resume-Text">Guitar Performance</div>
+	    				<div className="Resume-Text">Music Composition</div>
+	    				<div className="Resume-Text">User Interface Design</div>
+    			</div>
+    			<div className="Resume-Section">
     				<div className="Resume-Section-Header">Work Experience</div>
 
-    				<div className="Resume-Entry-Header">Sound Technologist at Meow Wolf [04/18-Present]</div>
-	    			<div className="Resume-Text">Work as a creative voice, programmer, and electrical engineer providing for sound technology needs at current and future installations, including:</div>
+    				<div className="Resume-Entry-Header">Sound Technologist - Meow Wolf [04/18-Present]</div>
+	    			<div className="Resume-Text">Provide for audio technology needs of large scale immersive art installations, including:</div>
 	    			<ul className="Resume-List">
-	    				<li className="Resume-List-Text">Creative concepting and decision making for audio tech in installation pieces, ranging from technical problem solving, to filling out specifics of broad ideas, to developing and executing original ideas.</li>
-	    				<li className="Resume-List-Text">Design of custom circuit boards, including a modular driver system for large-scale robotic orchestras. </li>
-	    				<li className="Resume-List-Text">Design of generative and interactive audio systems, including generative motor sound design for art car installations, spatial systems, and interactive systems.</li>
-	    				<li className="Resume-List-Text">Embedded audio design, including a multichannel audio system for the Teensy Microcontroller </li>
-	    				<li className="Resume-List-Text">Installation of audio tech in exhibitions, including tech for a room at the Meow Wolf House of
-							Eternal Return in Santa Fe, New Mexico.</li>
-	    				<li className="Resume-List-Text">Skills Engaged: Electrical Engineering, KiCad, Max/MSP, C/C++ (JUCE), QSYS, Embedded
-							Programming (Arduino), Sound Design, Creativity, Collaboration, Technical Problem Solving</li>
+	    				<li className="Resume-List-Text">Creative direction for interactive and generative audio</li>
+	    				<li className="Resume-List-Text">Collaborative work with other artists</li>
+	    				<li className="Resume-List-Text">Circuit board design</li>
+	    				<li className="Resume-List-Text">Software and firmware programming</li>
+	    				<li className="Resume-List-Text">Work on projects for Meow Wolf Denver, Meow Wolf Las Vegas, and the House of Eternal Return</li>
 	    			</ul>
 
-	    			<div className="Resume-Entry-Header">Design Engineer and Programmer at Dwarfcraft Devices [09/16-Present]</div>
-	    			<div className="Resume-Text">Work remotely as a programmer and hardware engineer on design of effects pedals and synthesizers, including:</div>
+	    			<div className="Resume-Entry-Header">Freelance Guitar Pedal Designer - Dwarfcraft Devices, J. Rockett Audio Designs [06/16-Present]</div>
+	    			<div className="Resume-Text">Design of guitar effects pedals, including:</div>
 	    			<ul className="Resume-List">
-	    				<li className="Resume-List-Text">Design and programming of DSP based effects pedals, including two designs completed and commercially released: The Ghost Fax Phase Computer and the Haxx II Ring Mod + Filter Pedal</li>
-	    				<li className="Resume-List-Text">Design and programming of effects for Dwarfcraft installations, including phaser and delay programs for an interactive exhibit at the Children’s Museum of Eau Claire.</li>
-						<li className="Resume-List-Text">Skills Engaged: C, Microcontroller Programming (Spin, PIC, STM32, ATMega, Sharc), KiCad, Electrical Engineering, Sound Design, Communication</li>
+	    				<li className="Resume-List-Text">Two commercially released designs: The 'Ghost Fax Phase Computer' and the 'Haxx II Ring Modulator / Filter Pedal'</li>
+	    				<li className="Resume-List-Text">Mixed signal circuit board design</li>
+						<li className="Resume-List-Text">Firmware implementation of DSP (Digital Signal Processing) algorithms</li>
+						<li className="Resume-List-Text">Creative design of audio effects</li>
 	    			</ul>
 
-    				<div className="Resume-Entry-Header">Design Engineer and Programmer at J. Rockett Audio Designs [08/17-04/18]</div>
-	    			<div className="Resume-Text">Worked remotely as a programmer and hardware engineer on design of effects pedals, including:</div>
-	    			<ul className="Resume-List">
-	    				<li className="Resume-List-Text">Design and programming of DSP emulations of classic analog effects, including digital emulation of analog tape delay.</li>
-	    				<li className="Resume-List-Text">Integration of analog and digital portions of designs, including modifying analog circuits such as preamps to work with digital elements, and designing a digital tap tempo system for analog delay.</li>
-	    				<li className="Resume-List-Text">Skills Engaged: C, Microcontroller Programming (Sharc), KiCad, Electrical Engineering, Sound Design, Communication</li>
-	    			</ul>
+    				<div className="Resume-Entry-Header">Guitarist - Elizabeth Colour Wheel [01/14-Present]</div>
+	    			<div className="Resume-Text">Play guitar with the internationally touring band Elizabeth Colour Wheel, signed to The Flenser record label.</div>
 
-	    			<div className="Resume-Entry-Header">Berklee Electronic Production and Design Department Tutor [01/17-07/17]</div>
-	    			<div className="Resume-Text">Worked as a tutor for the Electronic Production and Design (EPD) Department at Berklee College of Music including:</div>
-	    			<ul className="Resume-List">
-	    				<li className="Resume-List-Text">Tutoring students in the EPD major in all areas of the curriculum, including use of Logic, ProTools and Ableton, as well as programming in Max/MSP, C and Csound.</li>
-	    				<li className="Resume-List-Text">Tutoring first semester Berklee Students in concepts taught in music technology core curriculum including the use of Garageband and Reason. </li>
-	    				<li className="Resume-List-Text">Skills Engaged: Logic, Garageband, Ableton, Pro Tools, Reason, Max/MSP, C, Csound, Communication, Instructional Skills</li>
-	    			</ul>	    
-	    			<div className="Resume-Entry-Header">Programmer, Web Developer and Hardware Engineer at Point Motion [09/16-07/17]</div>			
-	    			<div className="Resume-Text">Worked on the development of Point Motion, a motion controlled music platform for music therapy, including:</div>
-	    			<ul className="Resume-List">
-	    				<li className="Resume-List-Text">Designing and programming audio features for the application, including effects rack with custom reverb, filter, delay and chorus effects. </li>
-	    				<li className="Resume-List-Text">Designing and programming a data tracking backend for the application, which allows therapists to track their patients progress remotely.</li>
-	    				<li className="Resume-List-Text">Designing and programming a web based front end for data tracker</li>
-	    				<li className="Resume-List-Text">Analog and digital design of a hardware controller for use with the application, and programming of the interface for communication between the application and the controller</li>
-	    				<li className="Resume-List-Text">Leading a team of interns in projects relating to hardware design</li>    				
-	    				<li className="Resume-List-Text">Skills Engaged: C++ (Including JUCE), JavaScript, React, Golang, SQL, Python, Linux, Electrical Engineering, Kicad, Communication, Team Leadership</li>
-	    			</ul>    			    			
-	    			<div className="Resume-Entry-Header">Design/Production Internship at Dwarfcraft Devices [06/16-09/16]</div>
-	    			<div className="Resume-Text">Worked as a paid design intern at Dwarfcraft Devices, including:</div>
-	    			<ul className="Resume-List">
-	    				<li className="Resume-List-Text">Beginning programming and design of modular synthesis project</li>
-	    				<li className="Resume-List-Text">Soldering and wiring effects pedals from schematic diagram in production line</li>
-	    				<li className="Resume-List-Text">Testing and repairing malfunctioning pedals returned by customers</li>
-	    				<li className="Resume-List-Text">Skills Engaged: C, Microcontroller Programming, KiCad, Electrical Engineering, Sound Design, Soldering, Communication</li>
-	    			</ul>
+	    			<div className="Resume-Entry-Header">The Sound of Dreaming [01/17-08/17]</div>
+	    			<div className="Resume-Text">Took part in the rehearsal and performance of an electronic dream opera entitled 'The Sound of Dreaming', which premiered at Moogfest 2017 with a repeat performance at Mass MoCA.</div>
 
-	    			<div className="Resume-Entry-Header">Experimental Music Reviewer at MXDWN [10/15-03/16]</div>
-    				<div className="Resume-Text">Provided critical reviews of current experimental music albums for entertainment website MXDWN, including a review of David Bowie’s final album, Blackstar</div>
-
-    				<div className="Resume-Entry-Header">Counselor at The Fantastic Magic Camp [06/09-06/12]</div>
-    				<div className="Resume-Text">Worked as a counselor at a non-profit summer camp specializing in inclusivity of special needs children, including:</div>
-    				<ul className="Resume-List">
-	    				<li className="Resume-List-Text">Directing a group of 10-15 campers in their daily activities and looking after their wellbeing</li>
-	    				<li className="Resume-List-Text">Responding to parental concerns related to the individual needs of their children</li>
-	    				<li className="Resume-List-Text">Skills Engaged: Team Leadership, Childcare, Providing for Special Needs Children, Communication</li>
-	    			</ul>
-    			</div>
-
-    			<div className="Resume-Section">
-	    			<div className="Resume-Section-Header">Performance Experience</div>
-	    			<div className="Resume-Entry-Header">Elizabeth Colour Wheel [2014-Present]</div>
-	    			<div className="Resume-Text">
-	    				Play guitar in a 5-piece band signed to The Flenser, on which we released our debut LP ‘Nocebo’ in March of 2019. We have toured throughout the United States and shared the stage with such acts as Have a Nice Life, Planning for Burial, Drowse, Full of Hell, Primitive Man, Jerome’s Dream, Loma Prieta, Big Brave, Sannhet, Street Sects, and more.
-	    			</div>
-
-	    			<div className="Resume-Entry-Header">The Sound of Dreaming [2017]</div>
-	    			<div className="Resume-Text">
-	    				Took part in the production and performance of a piece entitled the “The Sound of Dreaming”, headed by Nona Hendryx and Dr. Richard Boulanger, which premiered at Moogfest 2017 with a repeat performance at Mass MoCA. Contributions included developing a Csound based Vocal Effects Processor, which featured prominently in the piece, as well as performing on bass and DJ equipment, learning and managing several controller systems, and circuit-bending toys for use as sound-making props.
-	    			</div>
-	    			
-	    		</div>
-
-	    		    			<div className="Resume-Section">
-	    			<div className="Resume-Section-Header">Additional Experience</div>
-
-	    			<div className="Resume-Entry-Header">Reverb Plugin Design [2017]</div>
-	    			<div className="Resume-Text">Designed and programmed a Reverb Plugin using C++/JUCE as part of my thesis project at Berklee College of Music. </div>
-	    			
-	    		</div>
-
-    			<div className="Resume-Section">
-	    			<div className="Resume-Section-Header">Skills</div>
-	    			<div className="Resume-Entry-Header">Programming</div>
-	    			<div className="Resume-Text">
-	    				C, C++ (including JUCE Framwork), Max/MSP, Javascript (including React), Processing, Python, Linux (including Raspberry Pi), Assembly Languages, Includes Experience Programming Microcontrollers and Embedded Processors (Sharc DSP, SPIN FV-1, PIC32/16, STM32, ATMega, etc.)
-	    			</div>
-
-	    			<div className="Resume-Entry-Header">Software</div>	
-	    			<div className="Resume-Text">
-	    				Proficient with Ableton Live, Logic, Pro Tools, QSYS, Xcode, Unity, Sigma Studio, TrueStudio, MPLAB X, Premiere, After Effects, Finale, Sibelius, Microsoft Office, Photoshop, GIMP
-	    			</div>
-
-	    			<div className="Resume-Entry-Header">Music</div>	
-	    			<div className="Resume-Text">
-	    				Guitar, Vocals (lead and backup), Bass, Basic Keyboard, Production, Synthesis, Sound Design, DSP, Recording, Writing and Composition
-	    			</div>
-
-	    			<div className="Resume-Entry-Header">Electronics</div>	
-	    			<div className="Resume-Text">
-	    				Circuit Board Layout, Electronics Fabrication for Installations, Production Line Fabrication, Schematic and Circuit Board Reading, Oscilloscope Use, Effects Pedal Repair and Design, Guitar Electronics Repair
-	    			</div> 
-
-    			</div>   
+	    			<div className="Resume-Entry-Header">Electronic Production and Design Department Tutor - Berklee College of Music [01/17-06/17]</div>
+	    			<div className="Resume-Text">Worked as tutor helping students with all aspects of the Electronic Production and Design curriculum, including Audio Production Software, Audio Programming, and Sound Design.</div>
+    			</div> 
 
     			<div className="Resume-Section">
     				<div className="Resume-Section-Header">Education</div>
-    				<div className="Resume-Entry-Header">Berklee College of Music, Boston, MA [Graduated May 2017]</div>
-    				<div className="Resume-Text">BM in Electronic Production and Design with Minor in Acoustics and Electronics</div>
-    			</div>
-    			<div className="Resume-Section">
-    				<div className="Resume-Section-Header">Awards and Accolades</div>
-    				<div className="Resume-Entry-Header">Hearst Scholarship [2017]</div>
-    				<div className="Resume-Text">Awarded the Berklee Electronic Production and Design Department Hearst Scholarship through faculty recommendation. </div>
-    				<div className="Resume-Entry-Header">Berklee Achievement Scholarship [2015-2017]</div>
-    				<div className="Resume-Text">Awarded a scholarship in recognition of my musical, academic and extracurricular accomplishments at Berklee College of Music</div>
-    				<div className="Resume-Entry-Header">Dean’s List [2013-2017]</div>
-    				<div className="Resume-Text">Was selected for the Dean’s List at Berklee College of Music for every semester attended </div>
-    			</div> 			
+    				<div className="Resume-Entry-Header">Bachelor of Music - Electronic Production and Design - Berklee College of Music, Boston, MA [05/17]</div>
+    				<ul className="Resume-List">
+	    				<li className="Resume-List-Text">Designed a Reverb Plug-In using JUCE/C++ as thesis project</li>
+	    				<li className="Resume-List-Text">Guitar Principle Instrument, Minor in Acoustics and Electronics</li>
+						<li className="Resume-List-Text">Graduated Summa Cum Laude</li>
+	    			</ul>
+    			</div>			
     		</div>
     	</div>
     	);
