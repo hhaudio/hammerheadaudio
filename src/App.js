69a4d04a42cf6880b4e1dd3fe7fe6de76dd0389f
hhaudio <emmett@hammerheadaudio.com>
2020-03-19 14:20:15 -0500

    updated package.json

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      NavPage: "Designs",
+      NavPage: "Home",
       width: window.innerWidth,
       height: window.innerHeight
     }
